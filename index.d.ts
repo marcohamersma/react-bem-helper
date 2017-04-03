c6ee00e2c06a71f0d4ab58f6fa2ada5b8b6056a4
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-04-03 16:00:20 -0500

    Document typescript declarations

--- index.d.ts
+++ index.d.ts
@@ -5,68 +5,129 @@
  */
 
 interface BEMHelper<TReturn extends (string | BEMHelper.ReturnObject)> extends
-    BEMHelper.HelperFunction<TReturn> {}
+  BEMHelper.HelperFunction<TReturn> { }
 
 declare namespace BEMHelper {
-    interface PredicateList {
-        [key: string]: boolean | (() => boolean);
-    }
+  /**
+   * A mapping of strings to predicate functions or booleans.
+   *
+   * The values define whether the keys should be included in the list.
+   */
+  interface PredicateSet {
+    [key: string]: boolean | (() => boolean);
+  }
 
-    type List = string | string[] | PredicateList;
+  /**
+   * Several ways to define a set of strings words.
+   *
+   * It can be:
+   *   * A string of words separated by spaces.
+   *   * An array of space-separated word strings.
+   *   * A predicate set of space-separated word strings.
+   */
+  type WordSet = string | string[] | PredicateSet;
 
-    interface ElementArguments {
-        element?: string;
-    }
+  interface ElementArguments {
+    /**
+     * The name of the BEM element.
+     */
+    element?: string;
+  }
 
-    interface ModifierArguments extends ElementArguments {
-        modifier?: List;
-        modifiers?: List;
-    }
+  interface ModifierArguments extends ElementArguments {
+    /**
+     * A set of one or more modifiers.
+     */
+    modifier?: WordSet;
 
-    interface HelperArguments extends ModifierArguments {
-        extra?: List;
-    }
+    /**
+     * A set of one or more modifiers.
+     */
+    modifiers?: WordSet;
+  }
 
-    interface ReturnObject {
-        className: string;
-    }
+  interface HelperArguments extends ModifierArguments {
+    /**
+     * A set of extra plain classes to add without BEM prefixing.
+     */
+    extra?: WordSet;
+  }
 
-    interface HelperFunction<TReturn extends (string | ReturnObject)> {
-        (element?: string, modifiers?: List, extra?: List): TReturn;
-        (args: HelperArguments): TReturn;
-    }
+  interface ReturnObject {
+    className: string;
+  }
 
-    interface BaseConstructorOptions {
-        name: string;
-        prefix?: string;
-        modifierDelimiter?: string;
-    }
+  /**
+   * A function for creating BEM classes for a block.
+   */
+  interface HelperFunction<TReturn extends (string | ReturnObject)> {
+    /**
+     * @param {?string} element The name of the BEM element.
+     * @param {?List} modifiers A list of BEM modifiers to be applied to the element.
+     * @param {?List} extra A list of plain classes to add to the final classList.
+     */
+    (element?: string, modifiers?: WordSet, extra?: WordSet): TReturn;
+    (args: HelperArguments): TReturn;
+  }
 
-    interface StringConstructorOptions extends BaseConstructorOptions {
-        outputIsString: true;
-    }
+  interface BaseConstructorOptions {
+    /**
+     * The name of the BEM block.
+     */
+    name: string;
 
-    interface ObjectConstructorOptions extends BaseConstructorOptions {
-        outputIsString: false;
-    }
+    /**
+     * A string to prefix the block with.
+     */
+    prefix?: string;
 
-    type ConstructorOptions = StringConstructorOptions | ObjectConstructorOptions;
+    /**
+     * A string to use to separate the element name from the modifier.
+     */
+    modifierDelimiter?: string;
+  }
 
-    interface Constructor<TDefaultReturn extends (string | ReturnObject)> {
-        new(name: string): BEMHelper<TDefaultReturn>;
-        new(options: BaseConstructorOptions): BEMHelper<TDefaultReturn>;
-        new(options: StringConstructorOptions): BEMHelper<string>;
-        new(options: ObjectConstructorOptions): BEMHelper<ReturnObject>;
-        (name: string): BEMHelper<TDefaultReturn>;
-        (options: BaseConstructorOptions): BEMHelper<TDefaultReturn>;
-        (options: StringConstructorOptions): BEMHelper<string>;
-        (options: ObjectConstructorOptions): BEMHelper<ReturnObject>;
-    }
+  interface StringConstructorOptions extends BaseConstructorOptions {
+    /**
+     * Whether to return a string or an object containing a classList field.
+     */
+    outputIsString: true;
+  }
 
-    interface RootConstructor extends Constructor<ReturnObject> {
-        withDefaults(defaults: Partial<StringConstructorOptions>): Constructor<string>;
-        withDefaults(defaults: Partial<ObjectConstructorOptions>): Constructor<ReturnObject>;
-    }
+  interface ObjectConstructorOptions extends BaseConstructorOptions {
+    /**
+     * Whether to return a string or an object containing a classList field.
+     */
+    outputIsString: false;
+  }
+
+  type ConstructorOptions = StringConstructorOptions | ObjectConstructorOptions;
+
+  interface Constructor<TDefaultReturn extends (string | ReturnObject)> {
+    new (name: string): BEMHelper<TDefaultReturn>;
+    new (options: BaseConstructorOptions): BEMHelper<TDefaultReturn>;
+    new (options: StringConstructorOptions): BEMHelper<string>;
+    new (options: ObjectConstructorOptions): BEMHelper<ReturnObject>;
+    (name: string): BEMHelper<TDefaultReturn>;
+    (options: BaseConstructorOptions): BEMHelper<TDefaultReturn>;
+    (options: StringConstructorOptions): BEMHelper<string>;
+    (options: ObjectConstructorOptions): BEMHelper<ReturnObject>;
+  }
+
+  /**
+   * Create a new helper object for a BEM block.
+   */
+  interface RootConstructor extends Constructor<ReturnObject> {
+    /**
+     * Return a new constructor with the given defaults.
+     */
+    withDefaults(defaults: Partial<StringConstructorOptions>): Constructor<string>;
+
+    /**
+     * Return a new constructor with the given defaults.
+     */
+    withDefaults(defaults: Partial<ObjectConstructorOptions>): Constructor<ReturnObject>;
+  }
 }
 
 declare var BEMHelper: BEMHelper.RootConstructor;
